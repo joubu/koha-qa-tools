b8479e5189f992c62a828fd88e1a7cc2e1206835
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2016-11-01 09:31:16 +0000

    Lines should not terminate by semicolons in sysprefs.sql

--- QohA/File/Specific/Sysprefs.pm
+++ QohA/File/Specific/Sysprefs.pm
@@ -6,8 +6,11 @@ extends 'QohA::File';
 
 sub run_checks {
     my ($self, $cnt) = @_;
-    my @r = $self->check_sysprefs_order();
-    $self->SUPER::add_to_report('sysprefs_order', \@r);
+    my $r = $self->check_sysprefs_order();
+    $self->SUPER::add_to_report('sysprefs_order', $r);
+
+    $r = $self->check_semicolon($cnt);
+    $self->SUPER::add_to_report('semicolon', $r);
 
     return $self->SUPER::run_checks($cnt);
 }
@@ -42,6 +45,34 @@ sub check_sysprefs_order {
     return ( "Not blocker: Sysprefs $bad_placed is bad placed (see bug 10610)" );
 }
 
+sub check_semicolon {
+    my ($self, $cnt) = @_;
+
+    return 0 unless -e $self->path;
+
+    # For the first pass, I don't want to launch any test.
+    return 1 if $self->pass == 0;
+
+    my $git = QohA::Git->new();
+    my $diff_log = $git->diff_log($cnt, $self->path);
+    my @errors;
+    my $line_number = 1;
+    for my $line ( @$diff_log ) {
+        if ( $line =~ m|^@@ -\d+,{0,1}\d* \+(\d+),\d+ @@| ) {
+            $line_number = $1;
+            next;
+        }
+        next if $line =~ m|^-|;
+        $line_number++ and next unless $line =~ m|^\+|;
+        push @errors, "simicolon found instead of comma at line $line_number"
+            if $line =~ /;$/;
+        $line_number++;
+    }
+
+    return @errors
+        ? \@errors
+        : 1;
+}
 1;
 
 =head1 AUTHOR
